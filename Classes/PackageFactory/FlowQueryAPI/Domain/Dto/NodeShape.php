b8e556b543d07bac6925025c95c07ed9e67ccd84
Wilhelm Behncke <behncke@sitegeist.de>
2016-04-10 14:10:33 +0200

    TASK: Allow for white- and blacklisting in node shapes

--- Classes/PackageFactory/FlowQueryAPI/Domain/Dto/NodeShape.php
+++ Classes/PackageFactory/FlowQueryAPI/Domain/Dto/NodeShape.php
@@ -14,12 +14,15 @@ class NodeShape implements \JsonSerializable
     /**
      * @var array
      */
-    protected $shapeDescription;
+    protected $shapeDescription = [
+        '$included' => [],
+        '$excluded' => []
+    ];
 
     public function __construct(NodeInterface $node, array $shapeDescription = [])
     {
         $this->node = $node;
-        $this->shapeDescription = $shapeDescription;
+        $this->mergeShapeDescription($shapeDescription);
     }
 
     public function mergeShapeDescription(array $shapeDescription)
@@ -33,23 +36,46 @@ class NodeShape implements \JsonSerializable
 
         $topLevelPropertyNames = ObjectAccess::getGettablePropertyNames($this->node);
         foreach ($topLevelPropertyNames as $propertyName) {
-            if ($propertyName != 'properties' && !empty($this->shapeDescription) && isset($this->shapeDescription[$propertyName])) {
+            if (
+                $propertyName != 'properties' &&
+                $this->isWhiteListed($propertyName, $this->shapeDescription['$include']) &&
+                !$this->isBlackListed($propertyName, $this->shapeDescription['$exclude'])
+            ) {
                 $shape[$propertyName] = ObjectAccess::getProperty($node, $propertyName);
             }
         }
 
-        if (!empty($this->shapeDescription) && isset($this->shapeDescription['properties'])) {
+        if (
+            $this->isWhiteListed('properties', $this->shapeDescription['$include']) &&
+            !$this->isBlackListed('properties', $this->shapeDescription['$exclude'])
+        ) {
             $propertyNames = $this->node->getPropertyNames();
             $shape['properties'] = [];
 
             foreach ($propertyNames as $propertyName) {
-                if (!empty($this->shapeDescription['properties'][$propertyName])) {
+                if (
+                    $this->isWhiteListed($propertyName, $this->shapeDescription['$include']) &&
+                    !$this->isBlackListed($propertyName, $this->shapeDescription['$exclude'])
+                ) {
                     $property = $this->node->getProperty($propertyName);
 
                     if (is_array($property)) {
                         $shape['properties'][$propertyName] = $this->recursivelySerializeNodeProperties(
                             $property,
-                            $this->shapeDescription['properties'][$propertyName]
+
+                            //
+                            // Pass the whitelist, if existent - an empty array otherwise
+                            //
+                            isset($this->shapeDescription['$include']['properties']) &&
+                            isset($this->shapeDescription['$include']['properties'][$propertyName]) ?
+                            $this->shapeDescription['$include']['properties'][$propertyName] : [],
+
+                            //
+                            // Pass the blacklist, if existent - an empty array otherwise
+                            //
+                            isset($this->shapeDescription['$exclude']['properties']) &&
+                            isset($this->shapeDescription['$exclude']['properties'][$propertyName]) ?
+                            $this->shapeDescription['$exclude']['properties'][$propertyName] : []
                         );
                     }
 
@@ -61,16 +87,20 @@ class NodeShape implements \JsonSerializable
         return $shape;
     }
 
-    public function recursivelySerializeNodeProperties($shape['properties'][$propertyName], $shapeDescription)
+    protected function recursivelySerializeNodeProperties($shape['properties'][$propertyName], $whiteList, $blackList)
     {
         $result = [];
 
         foreach ($properties as $key => $value) {
-            if (isset($shapeDescription[$key])) {
+            if (
+                $this->isWhiteListed($key, $whiteList) &&
+                !$this->isBlackListed($key, $blackList)
+            ) {
                 if (is_array($value)) {
                     $result[$key] = $this->recursivelySerializeNodeProperties(
                         $value,
-                        $shapeDescription[$key]
+                        isset($whiteList[$key]) ? $whiteList[$key] : [],
+                        isset($blackList[$key]) ? $blackList[$key] : []
                     );
                 }
                 $result[$key] = $value;
@@ -78,5 +108,23 @@ class NodeShape implements \JsonSerializable
         }
 
         return $result;
+    }
+
+    protected function isWhiteListed($propertyName, $shapeDescription)
+    {
+        if (!isset($this->shapeDescription['$include'])) {
+            return true;
+        }
+
+        return isset($shapeDescription[$propertyName]);
+    }
+
+    protected function isBlackListed($propertyName, $shapeDescription)
+    {
+        if (!isset($this->shapeDescription['$exclude'])) {
+            return false;
+        }
+
+        return isset($shapeDescription[$propertyName]);
     }
 }
