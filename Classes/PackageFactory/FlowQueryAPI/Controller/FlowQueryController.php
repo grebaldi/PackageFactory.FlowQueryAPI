7ede72da2e9acb37a1eb87b637e351daefc8b5f2
Wilhelm Behncke <behncke@sitegeist.de>
2016-04-10 16:29:34 +0200

    TASK: Finalize FlowQueryController

--- Classes/PackageFactory/FlowQueryAPI/Controller/FlowQueryController.php
+++ Classes/PackageFactory/FlowQueryAPI/Controller/FlowQueryController.php
@@ -1,18 +1,57 @@
 <?php
 namespace PackageFactory\FlowQueryAPI\Controller;
 
+use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Eel\FlowQuery\FlowQuery;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use PackageFactory\FlowQueryAPI\Domain\Dto\NodeShape;
 
 class FlowQueryController extends APIController
 {
     /**
+     * @Flow\IgnoreValidation("$q")
      * @param FlowQuery $q
      * @param string $finisher
      * @param array $finisherArguments
      * @return void
      */
-    public function queryAction(FlowQuery $q, $finisher, array $finisherArguments)
+    public function queryAction(FlowQuery $q, $finisher, array $finisherArguments = [])
     {
+        $result = null;
 
+        switch($finisher) {
+            case 'get':
+            case 'count':
+            case 'property':
+            case 'is':
+                $result = call_user_func_array([$q, $finisher], $finisherArguments);
+                break;
+
+            default:
+                throw new \Exception(sprintf('%s is not allowed as a finisher.', $finisher), 1460298033);
+        }
+
+        $this->view->assign('value', $this->prepareResponse($result));
+    }
+
+    protected function prepareResponse($resource) {
+        if (is_array($resource)) {
+            $result = [];
+
+            foreach ($resource as $singleResource) {
+                $result[] = $this->prepareResponse($singleResource);
+            }
+
+            return $result;
+        }
+
+        if ($resource instanceof NodeInterface) {
+            $shape = new NodeShape($resource);
+            $shape->setControllerContext($this->controllerContext);
+
+            return $shape;
+        }
+
+        return $resource;
     }
 }
