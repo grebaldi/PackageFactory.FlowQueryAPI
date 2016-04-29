9f69f2902b2bd3fc011915266d09725d6586e902
Wilhelm Behncke <behncke@sitegeist.de>
2016-04-29 14:10:51 +0200

    TASK: Remove unnecessary bootstrapping

--- Resources/Private/JavaScript/index.js
+++ Resources/Private/JavaScript/index.js
@@ -1,7 +1,51 @@
-import createFlowQueryAPI from './api';
+import {
+	normalizeContextItem
+} from './Utils/index';
 
-export default createFlowQueryAPI({
-    endpoints: {
-        query: '/query'
-    }
-});
+import {operations} from './Operations/index';
+
+const createDispatch = configuration => request => {
+	return fetch(configuration.endpoints.query, {
+		method: 'POST',
+		body: JSON.stringify({
+			q: request,
+			finisher: request.finisher,
+			finisherArguments: request.finisherArguments
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	.then(res => res.json());
+};
+
+const initializeChainedInterface = (configuration, request) => Object.keys(operations).reduce(
+	(chainedInterface, operationName) => {
+		const operation = operations[operationName](configuration, request);
+		chainedInterface[operationName] = (...args) => {
+			const rechain = operation(...args);
+			return rechain(
+				request => initializeChainedInterface(configuration, request),
+				createDispatch(configuration)
+			);
+		};
+
+		return chainedInterface;
+	},
+	{}
+);
+
+export default configuration => {
+	const q = (context) => {
+		if (!Array.isArray(context)) {
+			context = [context];
+		}
+
+		return initializeChainedInterface(configuration, {
+			context: context.map(normalizeContextItem),
+			chain: []
+		});
+	};
+
+	return {q};
+};
